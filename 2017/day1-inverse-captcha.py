3ed4bc3c55f9444bdc305c591799470d62c8821a
Jonathan Murray <jon.lee.murray@gmail.com>
2017-12-03 13:27:18 -0400

    Compliant with python3 flake8 linter

--- 2017/day1-inverse-captcha.py
+++ 2017/day1-inverse-captcha.py
@@ -2,43 +2,46 @@
 # Author: JLeemur #####################
 # http://adventofcode.com/2017/day/1 ##
 #######################################
-# type(input) = str
-# return int
+
+
 def part1(input):
-	length = len(input)
-	sum = 0
-	prev = int(input[0])
+    # Args: str, Return: int
+    length = len(input)
+    sum = 0
+    prev = int(input[0])
 
-	for i in range(1,length):
-		curr = int(input[i])
-		if prev == curr:
-			sum += curr
-		prev = curr
+    for i in range(1, length):
+        curr = int(input[i])
+        if prev == curr:
+            sum += curr
+        prev = curr
 
-	if int(input[0]) == int(input[length-1]):
-		sum += int(input[0])
+    if int(input[0]) == int(input[length-1]):
+        sum += int(input[0])
 
-	return sum
+    return sum
 
-# type(input) str
-# return int
+
 def part2(input):
-	length = len(input)
-	sum = 0
-	steps = int(length/2)
+    # Args: str, Return: int
+    length = len(input)
+    sum = 0
+    steps = int(length/2)
 
-	for i in range(0,length):
-		curr = int(input[i])
-		steps_index = (i+steps) % length
-		steps_value = int(input[steps_index])
+    for i in range(0, length):
+        curr = int(input[i])
+        steps_index = (i+steps) % length
+        steps_value = int(input[steps_index])
 
-		if (curr == steps_value):
-			sum += curr
+        if (curr == steps_value):
+            sum += curr
 
-	return sum
+    return sum
+
 
 if __name__ == '__main__':
-	for line in open('day1.input'):
-		input = line.strip()
-	print("Part 1: " + str(part1(input)))
-	print("Part 2: " + str(part2(input)))
+    for line in open('day1.input'):
+        input = line.strip()
+
+    print("Part 1: " + str(part1(input)))
+    print("Part 2: " + str(part2(input)))
--- 2017/day3-spiral-memory.py
+++ 2017/day3-spiral-memory.py
@@ -3,19 +3,20 @@
 # http://adventofcode.com/2017/day/3 ##
 #######################################
 
-# type(input) = int
-# return int
-def part1(input):
-	return input
 
-# type(input) = int
-# return int
+def part1(input):
+    # Args: int, Return: int
+    return input
+
+
 def part2(input):
-	return input
+    # Args: int, Return: int
+    return input
+
 
 if __name__ == '__main__':
-	input = 0
-	for line in open('day3.input'):
-		input = line.strip()
-	print("Part 1: " + str(part1(input)))
-	print("Part 2: " + str(part2(input)))
+    for line in open('day3.input'):
+        input = line.strip()
+
+    print("Part 1: " + str(part1(input)))
+    print("Part 2: " + str(part2(input)))
